d4c4c53d2aca629de565fc71ec453a8a61bc84c9
renber <rene.bergelt@informatik.tu-chemnitz.de>
2016-07-04 10:19:45 +0200

    CredentialFormContent is now derived directly from HttpContent and not from FormUrlEncodedContent -> fixes SerializeToStreamAsync(...) not being called in Universal Windows Projects (UWP) and thus not being able to establish a session since no credentials were sent

--- SeafClient/Utils/CredentialFormContent.cs
+++ SeafClient/Utils/CredentialFormContent.cs
@@ -4,51 +4,60 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SeafClient.Utils
 {
     /// <summary>
-    /// A FormUrlEncodedContent implementation to transmit form data (key-value pairs)
+    /// A HttpContent implementation to transmit form data (key-value pairs)
     /// which zeroes out the value arrays as soon as the content has been written to a request stream
+    /// (Implementation of FormUrlEncodedContent which does not leak values in memory after the request has been sent)
     /// </summary>
-    class CredentialFormContent : FormUrlEncodedContent
+    class CredentialFormContent : HttpContent
     {
         List<KeyValuePair<string, char[]>> FormData;
 
         public CredentialFormContent(params KeyValuePair<string, char[]>[] formData)
-            : base(new KeyValuePair<string,string>[0])
+            : base()
         {
+            this.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+
             FormData = new List<KeyValuePair<string, char[]>>();
             foreach(var kv in formData)
             {
                 var pair = new KeyValuePair<string, char[]>(kv.Key, new char[kv.Value.Length]);
                 Array.Copy(kv.Value, pair.Value, kv.Value.Length);
                 FormData.Add(pair);
-            }
-        }
+            }            
+        }        
 
         protected override async Task SerializeToStreamAsync(Stream stream, TransportContext context)
         {
             try
             {
-                byte[] andBuf = Encoding.UTF8.GetBytes("&");
-
-                for (int i = 0; i < FormData.Count; i++)
-                {
-                    var pair = FormData[i];
-                    // enumerate parameters with &
-                    if (i > 0)
-                        await stream.WriteAsync(andBuf, 0, andBuf.Length);
-
-                    // write key=value
-                    await WritePairToStream(stream, pair);
-                }
+                await WriteFormDataToStream(stream);
             }
             finally
             {
                 ClearPassword();
+            }
+        }        
+
+        async Task WriteFormDataToStream(Stream stream)
+        {
+            byte[] andBuf = Encoding.UTF8.GetBytes("&");
+
+            for (int i = 0; i < FormData.Count; i++)
+            {
+                var pair = FormData[i];
+                // enumerate parameters with &
+                if (i > 0)
+                    await stream.WriteAsync(andBuf, 0, andBuf.Length);
+
+                // write key=value
+                await WritePairToStream(stream, pair);
             }
         }
 
@@ -87,9 +96,9 @@ namespace SeafClient.Utils
         }
 
         protected override bool TryComputeLength(out long length)
-        {
+        { 
             length = -1;
-            return false;
+            return false;           
         }
     }
 }
