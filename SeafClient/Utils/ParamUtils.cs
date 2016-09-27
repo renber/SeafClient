9999246d03e159a25ddf8a562e101d7068a8afa4
renber <rene.bergelt@informatik.tu-chemnitz.de>
2016-09-27 17:03:23 +0200

    Added ParamUtils with parameter null check convenience method

---
+++ SeafClient/Utils/ParamUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeafClient.Utils
+{
+    public static class ParamUtils
+    {
+        /// <summary>
+        /// Throws a ArgumentNullException if the given object is null
+        /// </summary>
+        /// <param name="param"></param>
+        /// <param name="parameterName">The parameterName to use in the ArgumentNullException</param>
+        public static void ThrowOnNull(this Object param, String parameterName)
+        {
+            if (param == null)
+                throw new ArgumentNullException("The parameter " + parameterName + " must not be null.");
+        }
+
+    }
+}
