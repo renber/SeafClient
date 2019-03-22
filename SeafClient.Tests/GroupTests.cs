2892f78d32f72fd28064c63651a77190c8000bac
René Bergelt <rene.bergelt@informatik.tu-chemnitz.de>
2019-03-22 10:18:39 +0100

    Added Test for BulkAddGroupMember

--- SeafClient.Tests/GroupTests.cs
+++ SeafClient.Tests/GroupTests.cs
@@ -65,6 +65,56 @@ namespace SeafClient.Tests
             Assert.IsTrue(request.WasSuccessful(message));
             var result = ExecuteSync(() => request.ParseResponseAsync(message));
             Assert.AreEqual(3, result);
-        }        
+        }
+
+        [TestMethod]
+        public void Test_BulkAddGroupMember_Success()
+        {
+            var request = new BulkAddGroupMemberRequest(FakeToken, 42, new List<string> { "new-member-1@email.com", "new-member-2@email.com","new-member-3@email.com", "new-member-4@email.com" });
+
+            var message = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(
+            @"{
+            ""failed"":[
+            {
+                    ""error_msg"": ""Invalid email"",
+                ""email"": ""new-member-3@email.com""
+            },
+            {
+                    ""error_msg"": ""Is already group member"",
+                ""email"": ""new-member-4@email.com""
+            }],
+             ""success"":[
+                    {
+                ""login_id"": """",
+                        ""name"": ""new-member-1"",
+                        ""avatar_url"": ""https://cloud.seafile.com/media/avatars/default.png"",
+                        ""is_admin"": false,
+                        ""contact_email"": ""new-member-1@email.com"",
+                        ""email"": ""new-member-1@email.com""
+            },
+            {
+                ""login_id"": """",
+                ""name"": ""new-member-2"",
+                ""avatar_url"": ""https://cloud.seafile.com/media/avatars/default.png"",
+                ""is_admin"": false,
+                ""contact_email"": ""new-member-2@email.com"",
+                ""email"": ""new-member-2@email.com""
+            }]}")
+                };
+
+            Assert.IsTrue(request.WasSuccessful(message));
+            var result = ExecuteSync(() => request.ParseResponseAsync(message));
+
+            Assert.AreEqual(2, result.Failed.Length);
+            Assert.AreEqual(2, result.Success.Length);
+
+            Assert.AreEqual("new-member-3@email.com", result.Failed.First().Email);
+            Assert.AreEqual("Invalid email", result.Failed.First().ErrorMessage);
+
+            Assert.AreEqual("new-member-2@email.com", result.Success.Skip(1).First().Email);
+            Assert.AreEqual("new-member-2", result.Success.Skip(1).First().Name);
+        }
     }
 }
